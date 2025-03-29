3ad2215945fe44dfa3af4ae8eef2e83f83f6e406
GergesHany <gergeshany179@gmail.com>
2025-03-29 16:37:49 +0200

    added Start TCP server and handle Connection

--- main.go
+++ main.go
@@ -2,8 +2,46 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net"
+	"os"
 )
 
+const defaultAddress = "0.0.0.0:7000"
+
 func main() {
-	fmt.Println("Starting")
+	l, err := net.Listen("tcp", defaultAddress)
+	if err != nil {
+		fmt.Printf("Error starting server: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Starting server at", defaultAddress)
+
+	// Listen for inputs and respond
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Printf("Error accepting connection: %v\n", err)
+			continue
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	buf := make([]byte, 2014) // store out stuff somewhere
+
+	for {
+		len, err := conn.Read(buf)
+		if err != nil {
+			if err != io.EOF {
+				fmt.Printf("Error reading: %#v\n", err)
+			}
+			break
+		}
+		command := buf[:len]
+		fmt.Print("Message received: ", string(command))
+	}
 }
